3f09a7b00d9ddb7f541621e7bc7b717c9d704197
Mohib Alvi <mohibtech@gmail.com>
2019-05-07 07:57:44 +0500

    Update SSIS_PKG_RUN_TSQL.sql

--- MSSQL/SSIS_PKG_RUN_TSQL.sql
+++ MSSQL/SSIS_PKG_RUN_TSQL.sql
@@ -1,15 +1,20 @@
 Declare @execution_id bigint
+
 EXEC [SSISDB].[catalog].[create_execution] @package_name=N'Package.dtsx',
     @execution_id=@execution_id OUTPUT,
     @folder_name=N'Deployed Projects',
 	  @project_name=N'Integration Services Project1',
   	@use32bitruntime=False,
 	  @reference_id=Null
+
 Select @execution_id
+
 DECLARE @var0 smallint = 1
+
 EXEC [SSISDB].[catalog].[set_execution_parameter_value] @execution_id,
     @object_type=50,
 	  @parameter_name=N'LOGGING_LEVEL',
 	  @parameter_value=@var0
+
 EXEC [SSISDB].[catalog].[start_execution] @execution_id
 GO
