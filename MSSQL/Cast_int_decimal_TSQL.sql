2a820beaa3957d52d5537d9b979d8d0fb5514cc9
Mohib Alvi <mohibtech@gmail.com>
2019-05-06 23:39:31 -0700

    Create Cast_int_decimal_TSQL.sql

---
+++ MSSQL/Cast_int_decimal_TSQL.sql
@@ -0,0 +1,29 @@
+DECLARE @CNTNULL decimal(10,2);
+DECLARE @TOTCNT decimal(10,2);
+DECLARE @TotPercent decimal(10,2);
+
+select @TOTCNT = count(*)
+from [dbo].[FactDemandWareOrderHeaderDiscounts];
+
+select @CNTNULL = count(*)
+from [dbo].[FactDemandWareOrderHeaderDiscounts]
+where PromotionID = '' --is null
+
+set @TotPercent = @CNTNULL/@TOTCNT * 100 ;
+select @TOTCNT "TotalCount", @CNTNULL "NullCount" , @TotPercent as "Total Percentage"
+
+OR 
+
+DECLARE @CNTNULL int;
+DECLARE @TOTCNT int;
+DECLARE @TotPercent decimal(10,2);
+
+select @TOTCNT = count(*)
+from [dbo].[FactDemandWareOrderHeaderDiscounts];
+
+select @CNTNULL = count(*)
+from [dbo].[FactDemandWareOrderHeaderDiscounts]
+where PromotionID = '' --is null
+
+set @TotPercent = cast(@CNTNULL as decimal(10,2))/cast(@TOTCNT as decimal(10,2)) * 100 ;
+select @TOTCNT "TotalCount", @CNTNULL "NullCount" , @TotPercent as "Total Percentage"
